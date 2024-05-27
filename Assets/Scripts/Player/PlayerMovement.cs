018b21ecaae5af9afd274954d101e3d7639e4319
Johannes Fagerberg <62025008+johfager@users.noreply.github.com>
2024-05-27 12:50:05 +0200

    Add launch point that rotates around player, buggy at the moment

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.Events;
 
 namespace Player
 {
@@ -10,6 +10,9 @@ namespace Player
 
         private Rigidbody2D _rb;
         private bool _grounded;
+
+        // Unity event to be invoked when the player changes direction
+        public UnityEvent<GameObject, Vector3> onDirectionChange = new UnityEvent<GameObject, Vector3>();
 
         void Start()
         {
@@ -35,7 +38,19 @@ namespace Player
         {
             if (collision.gameObject.CompareTag("Edge"))
             {
+                // Change direction
                 speed = -speed;
+                
+                // Invoke the Unity event with the player GameObject as an argument
+                if(speed > 0)
+                {
+                    onDirectionChange.Invoke(gameObject, Vector3.right);
+                }
+                else
+                {
+                    onDirectionChange.Invoke(gameObject, Vector3.left);
+                }
+ 
             }
 
             if (collision.gameObject.CompareTag("Floor"))
